eb93fa1447dd549a1d76a0ed84f9b3f5c40982d5
Jan Staelens <jan.staelens@skyline.be>
2023-03-20 16:48:10 +0100

    Initial Solution

---
+++ Empower.Library.RoomTest/OrderFactory.cs
@@ -0,0 +1,24 @@
+﻿using Skyline.DataMiner.Empower.Library.RoomTest;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Empower.Library.RoomTest
+{
+    /// <summary>
+    /// Factory to create Orders.
+    /// </summary>
+    public static class OrderFactory
+    {
+        /// <summary>
+        /// Create an instance of <see cref="IOrder"/>.
+        /// </summary>
+        /// <param name="orderValue">Value of the order.</param>
+        /// <returns>An instance of <see cref="IOrder"/>.</returns>
+        public static IOrder CreateOrder(string orderValue)
+        {
+            return new Order(orderValue);
+        }
+    }
+}
